ca80a099479fd1398428f5a6a281a2f370ecf5db
kamal heydari <kamal3heydari@gmail.com>
2021-10-29 11:10:12 +0330

    Updates

---
+++ precache-manifest.daae2fd95d7b867e71a489354feebebe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cd218b162c205c130c6adfaebde4bb9",
+    "url": "/react-github-users/index.html"
+  },
+  {
+    "revision": "6cb2a9867317691fd880",
+    "url": "/react-github-users/static/css/main.0d1d684e.chunk.css"
+  },
+  {
+    "revision": "3615be8877b82f2eaa86",
+    "url": "/react-github-users/static/js/2.db8c7841.chunk.js"
+  },
+  {
+    "revision": "dd5958a82507b7f6fca3ce3dc2fb279b",
+    "url": "/react-github-users/static/js/2.db8c7841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cb2a9867317691fd880",
+    "url": "/react-github-users/static/js/main.fcca4cdf.chunk.js"
+  },
+  {
+    "revision": "b39e77899cdaff5bea52",
+    "url": "/react-github-users/static/js/runtime-main.cc994b05.js"
+  },
+  {
+    "revision": "e5aa8fdf93548cd5922effd537a71211",
+    "url": "/react-github-users/static/media/git3.e5aa8fdf.mp4"
+  }
+]);
